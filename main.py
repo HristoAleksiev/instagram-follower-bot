55a8d29b3702be0e4a9ccf8177e17d60d5535401
Алексиев <kadoll555@gmail.com>
2021-06-12 22:12:48 +0300

    Comments

--- main.py
+++ main.py
@@ -13,11 +13,9 @@ chrome_driver_path = "C:\Python Stuff\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 site = "https://www.instagram.com/accounts/login/"
-
 driver.get(url=site)
 
 target_account = "instagram"
-
 driver.find_element_by_xpath('/html/body/div[3]/div/div/button[1]').click()
 
 # Login
@@ -42,19 +40,22 @@ WebDriverWait(driver, 10). \
                                                  'div/div[2]/div/div[1]/a'))) \
     .click()
 
-# get to followers popup
+# get the followers popup window:
 WebDriverWait(driver, 10). \
     until(condition.presence_of_element_located((By.XPATH,
                                                  '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a'))) \
     .click()
 
 popup_body = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]')
+
+# load the list of [visible] accounts that follow the opened page
 WebDriverWait(driver, 10). \
     until(condition.presence_of_element_located((By.XPATH,
                                                  '/html/body/div[5]/div/div/div[2]/ul/div')))
 follow_list = driver.find_elements_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li')
-
 position = 0
+
+# go through all the visible accounts and click on "Follow"
 while True:
     for itr in range(7):
         follow_button = follow_list[position].find_element_by_tag_name("button")
@@ -62,6 +63,8 @@ while True:
         if follow_button.text == "Follow":
             follow_button.click()
         position += 1
+    # will have to keep scrolling the popup window to load more accounts and add them to the list,
+    # otherwise they do not exist in the DOM
     driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;", popup_body)
     t.sleep(2)
     follow_list = driver.find_elements_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li')
